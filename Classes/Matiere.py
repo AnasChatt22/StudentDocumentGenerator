8c6416157e247e2aabcd1395f4ff8c865f15a1c8
AnasChatt22 <chattanas22@gmail.com>
2023-12-08 14:32:31 +0100

    commit test

---
+++ Classes/Matiere.py
@@ -0,0 +1,5 @@
+class Matiere:
+    def __init__(self, ID_matiere, designation, nbr_heure):
+        self.ID_matiere = ID_matiere
+        self.designation = designation
+        self.nbr_heure = nbr_heure
