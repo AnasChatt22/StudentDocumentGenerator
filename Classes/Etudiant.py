49ec446efff3594fb17438a6c60d39d64edaaba8
AnasChatt22 <chattanas22@gmail.com>
2023-12-08 22:02:34 +0100

    some changes

--- Classes/Etudiant.py
+++ Classes/Etudiant.py
@@ -1,16 +1,16 @@
 from enum import Enum
 
+
 class SEXE(Enum):
     HOMME = "HOMME"
     FEMME = "FEMME"
 
+
 class Etudiant:
-    def __init__(self, id_etud, nom, prenom, email,sexe,date_naissance):
+    def __init__(self, id_etud, nom, prenom, email, sexe, date_naissance):
         self.id_etud = id_etud
         self.nom = nom
         self.prenom = prenom
         self.email = email
-        self.sexe= sexe
+        self.sexe = sexe
         self.date_naissance = date_naissance
-
-
---
+++ Classes/Note.py
@@ -0,0 +1,5 @@
+class Note:
+    def __init__(self, etudiant, matiere, note):
+        self.etudiant = etudiant
+        self.matiere = matiere
+        self.note = note
