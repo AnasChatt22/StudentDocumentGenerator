2a2d8a924ec769c3f78e8e2874fe9c08122f03d5
Oussama Bentoufile <Oussa2ben7@gmail.com>
2023-12-08 21:46:45 +0100

    Modified Classes

--- Classes/Module.py
+++ Classes/Module.py
@@ -1,6 +1,7 @@
 class Module:
-    def __init__(self, ID_module, designation, responsable, nbr_heure):
-        self.ID_module = ID_module
+    def __init__(self, id_module, designation, responsable, nombre_heure, semestre):
+        self.id_module = id_module
         self.designation = designation
         self.responsable = responsable
         self.nbr_heure = nbr_heure
+        self.semestre = semestre
