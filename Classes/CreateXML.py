619ca83159e1f9efcda989cdd5a371c320ba7ffb
AnasChatt22 <chattanas22@gmail.com>
2023-12-13 19:46:43 +0100

    Optimized code

--- Classes/CreateXML.py
+++ Classes/CreateXML.py
@@ -1,195 +1,136 @@
-import pandas as pd
-import xml.etree.ElementTree as ET
+import random
+import traceback
 import xml.dom.minidom
-from Etudiant import Etudiant
-from Matiere import Matiere
-from Module import Module
-from Note import Note
-import ValidateXML
+import xml.etree.ElementTree as ET
+
+import pandas as pd
 
 
-class CreateXML:
-    def __init__(self, Excel_Path_Etudiants, Excel_Path_Modules, Excel_Path_Notes, Excel_Path_Emploi, XML_Path_GINF2,
-                 XML_Path_Emploi):
-        self.etudiants_xl_path = Excel_Path_Etudiants
-        self.modules_xl_path = Excel_Path_Modules
-        self.notes_xl_path = Excel_Path_Notes
-        self.emploi_xl_path = Excel_Path_Emploi
-        self.xml_path_GINF2 = XML_Path_GINF2
-        self.xml_path_Emploi = XML_Path_Emploi
-        self.etudiants = []
-        self.modules = []
-        self.notes = []
+def Creer_xml_GINF2(Excel_Path_Etudiants, Excel_Path_Modules, XML_Path_GINF2):
+    try:
+        # Read Excel files
+        read_xl_etudiant = pd.read_excel(Excel_Path_Etudiants)
+        read_xl_module = pd.read_excel(Excel_Path_Modules)
 
-    def ajout_etudiants(self):
-        try:
-            read_xl = pd.read_excel(self.etudiants_xl_path)
-            for _, row in read_xl.iterrows():
-                date_naissance = pd.to_datetime(row['date_naissance'])
-                etudiant = Etudiant(row['id_etudiant'], row['nom'], row['prenom'], row['email'], row['sexe'],
-                                    date_naissance)
-                self.etudiants.append(etudiant)
-        except Exception as ex:
-            print(f"Erreur d'ajout des étudiants: {ex}")
+        # Root element for the XML document
+        xml_doc = ET.Element("Etudiants")
 
-    def ajout_modules(self):
-        try:
-            read_xl = pd.read_excel(self.modules_xl_path)
-            for _, row in read_xl.iterrows():
-                module = Module(row['id_module'], row['designation_module'], row['responsable'], row['semestre'])
-                self.modules.append(module)
+        # Iterate over each row in the Etudiants Excel file
+        for _, row_etd in read_xl_etudiant.iterrows():
+            # Convert the date of birth to a datetime object
+            date_naissance = pd.to_datetime(row_etd['date_naissance'])
 
+            # Create an Etudiant element with attributes
+            etudiant_element = ET.SubElement(xml_doc, "Etudiant", id_etudiant=str(row_etd['id_etudiant']),
+                                             sexe=str(row_etd['sexe']),
+                                             date_naissance=str(date_naissance.strftime("%Y-%m-%d")))
+
+            # Add sub-elements for Nom, Prenom, and Email under Etudiant
+            ET.SubElement(etudiant_element, "Nom").text = row_etd['nom']
+            ET.SubElement(etudiant_element, "Prenom").text = row_etd['prenom']
+            ET.SubElement(etudiant_element, "Email").text = row_etd['email']
+
+            # Iterate over each row in the Modules Excel file
+            for _, row_module in read_xl_module.iterrows():
+                # Create a Module element with attributes
+                module_element = ET.SubElement(etudiant_element, "Module", id_module=str(row_module['id_module']))
+                ET.SubElement(module_element, "Designation").text = row_module['designation_module']
+                ET.SubElement(module_element, "Responsable").text = row_module['responsable']
+                ET.SubElement(module_element, "Semestre").text = str(row_module['semestre'])
+
+                # Iterate over three potential Matieres
                 for i in range(1, 4):
-                    matiere_id = row[f'id_matiere_{i}']
-                    designation = row[f'designation_matiere_{i}']
-                    if not pd.isna(designation):
-                        matiere = Matiere(matiere_id, designation)
-                        module.matieres.append(matiere)
-        except Exception as ex:
-            print(f"Erreur d'ajout des modules: {ex}")
+                    # Check if the id_matiere_i column is not empty
+                    if pd.notna(row_module[f'id_matiere_{i}']):
+                        # Create a Matiere element with attributes
+                        matiere_element = ET.SubElement(module_element, "Matiere",
+                                                        id_matiere=str(row_module[f'id_matiere_{i}']))
+                        ET.SubElement(matiere_element, "Designation").text = row_module[f'designation_matiere_{i}']
 
-    def ajout_notes(self):
-        try:
-            for module in self.modules:
-                sheet = pd.read_excel(self.notes_xl_path, sheet_name=str(module.id_module))
+                        # Generate a random Note for Matiere
+                        ET.SubElement(matiere_element, "Note").text = str(round(random.uniform(2.0, 20.0)))
 
-                for _, row in sheet.iterrows():
-                    id_etudiant = row['id_etudiant']
-                    id_matiere = row['id_matiere']
-                    note_value = row['note']
+        # Format XML file
+        xml_string = ET.tostring(xml_doc, encoding="UTF-8")
+        dom = xml.dom.minidom.parseString(xml_string)
+        prettified_xml = dom.toprettyxml(indent="    ")
 
-                    etudiant = next((etudiant for etudiant in self.etudiants if etudiant.id_etudiant == id_etudiant),
-                                    None)
-                    matiere = next((matiere for matiere in module.matieres if matiere.id_matiere == id_matiere), None)
+        # Create XML File
+        with open(XML_Path_GINF2, "w", encoding="UTF-8") as generatedXML:
+            generatedXML.write(prettified_xml)
 
-                    if etudiant is not None and matiere is not None:
-                        note = Note(etudiant, matiere, note_value)
-                        self.notes.append(note)
-        except Exception as ex:
-            print(f"Erreur d'ajout des notes: {ex}")
+        print("Fichier GINF2.xml créé avec succès")
+    except Exception as ex:
+        traceback_str = traceback.format_exc()
+        print(f"Erreur de création du fichier GINF2.xml: {ex}\n{traceback_str}")
 
-    def Creer_xml_GINF2(self):
-        try:
-            xml_doc = ET.Element("Etudiants")
 
-            for etudiant in self.etudiants:
-                etudiant_element = ET.SubElement(xml_doc, "Etudiant", id_etudiant=str(etudiant.id_etudiant),
-                                                 sexe=str(etudiant.sexe),
-                                                 date_naissance=str(etudiant.date_naissance.strftime("%Y-%m-%d")))
+def Creer_xml_Emploi(Excel_Path_Emplois, Excel_Path_Modules, XML_Path_Emploi):
+    global semaine_element, jour_element, designation
+    try:
+        read_xl_emplois = pd.ExcelFile(Excel_Path_Emplois)
+        read_xl_module = pd.read_excel(Excel_Path_Modules)
 
-                ET.SubElement(etudiant_element, "Nom").text = etudiant.nom
-                ET.SubElement(etudiant_element, "Prenom").text = etudiant.prenom
-                ET.SubElement(etudiant_element, "Email").text = etudiant.email
+        xml_doc = ET.Element("Mi-semestres")
+        j = 0
 
-                for module in self.modules:
-                    module_element = ET.SubElement(etudiant_element, "Module", id_module=str(module.id_module))
-                    ET.SubElement(module_element, "Designation").text = module.designation
-                    ET.SubElement(module_element, "Responsable").text = module.responsable
-                    ET.SubElement(module_element, "Semestre").text = str(module.semestre)
+        for sheet in read_xl_emplois.sheet_names:
+            j += 1
+            mi_semestre_element = ET.SubElement(xml_doc, "Mi_semestre", numero=str(j))
+            sheet_data = pd.read_excel(read_xl_emplois, sheet)
 
-                    for matiere in module.matieres:
-                        matiere_element = ET.SubElement(module_element, "Matiere", id_matiere=str(matiere.id_matiere))
-                        ET.SubElement(matiere_element, "Designation").text = matiere.designation
+            for _, row in sheet_data.iterrows():
+                salle = "" if pd.isna(row['salle']) else row['salle']
+                professeur = "" if pd.isna(row['professeur']) else row['professeur']
+                seance_type = "" if pd.isna(row['type']) else row['type']
 
-                        for note in self.notes:
-                            if note.etudiant.id_etudiant == etudiant.id_etudiant and note.matiere.id_matiere == matiere.id_matiere:
-                                ET.SubElement(matiere_element, "Note").text = str(note.note)
-
-            # Format xml file
-            xml_string = ET.tostring(xml_doc, encoding="UTF-8")
-            dom = xml.dom.minidom.parseString(xml_string)
-            prettified_xml = dom.toprettyxml(indent="    ")
-
-            # Create XML File
-            with open(self.xml_path_GINF2, "w", encoding="UTF-8") as generatedXML:
-                generatedXML.write(prettified_xml)
-            print("Fichier GINF2.xml créé avec succés")
-        except Exception as ex:
-            print(f"Erreur de création du fichier GINF2.xml: {ex}")
-
-    def Creer_xml_Emploi(self):
-        try:
-            xml_doc = ET.Element("Mi-semestres", )
-
-            for i in range(1, 5):
-                sheet = pd.read_excel(self.emploi_xl_path, sheet_name="mi_semestre" + str(i))
-                mi_semestre_element = ET.SubElement(xml_doc, "Mi_semestre", numero=str(i))
-
-                semaines = set()
-                jours = set()
-                cp = 0
-
-                for _, row in sheet.iterrows():
-                    semaine = row['semaine']
-                    jour = row['jour']
-                    horaire = row['horaire']
-                    salle = "" if pd.isna(row['salle']) else row['salle']
-                    id_matiere = row['id_matiere']
-                    if pd.isna(id_matiere):
-                        designation = ""
-                    else:
-                        for module in self.modules:
-                            for matiere in module.matieres:
-                                if matiere.id_matiere == id_matiere:
-                                    designation = matiere.designation
+                if pd.isna(row['id_matiere']):
+                    designation = ""
+                else:
+                    for _, row_module in read_xl_module.iterrows():
+                        for i in range(1, 4):
+                            if pd.notna(row_module[f'id_matiere_{i}']):
+                                if row_module[f'id_matiere_{i}'] == row['id_matiere']:
+                                    designation = row_module[f'designation_matiere_{i}']
                                     break
-                    professeur = "" if pd.isna(row['professeur']) else row['professeur']
-                    seance_type = row['type']
-                    if pd.isna(seance_type):
-                        seance_type = ""
 
-                    if semaine not in semaines:
-                        semaine_element = ET.SubElement(mi_semestre_element, "Semaine", numero=str(semaine))
-                        semaines.add(semaine)
-                    if jour == "Vendredi":
-                        cp = cp + 1
-                    if jour not in jours:
-                        jour_element = ET.SubElement(semaine_element, "Jour", nom=jour)
-                        jours.add(jour)
+                if not pd.isna(row['premiere_semaine']):
+                    semaine_element = ET.SubElement(mi_semestre_element, "Semaine",
+                                                    Premiere=str(int(row['premiere_semaine'])),
+                                                    Derniere=str(int(row['derniere_semaine'])))
 
-                    seance_element = ET.SubElement(jour_element, "Seance", horaire=horaire, type=seance_type)
-                    matiere_element = ET.SubElement(seance_element, "Matière", designation=designation)
-                    ET.SubElement(matiere_element, "Professeur").text = professeur
-                    ET.SubElement(matiere_element, "Salle").text = salle
+                if not pd.isna(row['jour']):
+                    jour_element = ET.SubElement(semaine_element, "Jour", nom=row['jour'])
 
-                    if cp == 4:
-                        cp = 0
-                        jours = set()
+                seance_element = ET.SubElement(jour_element, "Seance", horaire=row['horaire'], type=seance_type)
+                matiere_element = ET.SubElement(seance_element, "Matière")
+                ET.SubElement(matiere_element, "Designation").text = designation
+                ET.SubElement(matiere_element, "Professeur").text = professeur
+                ET.SubElement(matiere_element, "Salle").text = salle
 
-            # Format xml file
-            xml_string = ET.tostring(xml_doc, encoding="UTF-8")
-            dom = xml.dom.minidom.parseString(xml_string)
-            prettified_xml = dom.toprettyxml(indent="    ")
+        # Format xml file
+        xml_string = ET.tostring(xml_doc, encoding="UTF-8")
+        dom = xml.dom.minidom.parseString(xml_string)
+        prettified_xml = dom.toprettyxml(indent="    ")
 
-            # Create XML File
-            with open(self.xml_path_Emploi, "w", encoding="UTF-8") as generatedXML:
-                generatedXML.write(prettified_xml)
-            print("Fichier Emploi.xml du temps créé avec succès")
-        except Exception as ex:
-            print(f"Erreur de création du fichier Emploi.xml: {ex}")
+        # Create XML File
+        with open(XML_Path_Emploi, "w", encoding="UTF-8") as generatedXML:
+            generatedXML.write(prettified_xml)
+
+        print("Fichier Emploi.xml créé avec succès")
+    except Exception as ex:
+        print(f"Erreur de création du fichier Emploi.xml: {ex}")
 
 
 if __name__ == "__main__":
-    # Edit paths before testing
+    # Example usage
     etudiant_xl_path = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersExcel\\Etudiants.xlsx"
     modules_xl_path = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersExcel\\Modules.xlsx"
-    notes_xl_path = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersExcel\\Notes.xlsx"
     emploi_xl_path = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersExcel\\Emplois.xlsx"
+
     xml_path_GINF2 = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersXML\\XML\\GINF2.xml"
     xml_path_Emploi = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersXML\\XML\\Emploi.xml"
-    dtd_path_GINF2 = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersXML\\DTD\\GINF2.dtd"
-    xsd_path_GINF2 = "C:\\Users\\AnasChatt\\Desktop\\XMLProjet\\FichiersXML\\XSD\\GINF2.xsd"
 
-    try:
-        xml_file = CreateXML(etudiant_xl_path, modules_xl_path, notes_xl_path, emploi_xl_path, xml_path_GINF2,
-                             xml_path_Emploi)
-        xml_file.ajout_etudiants()
-        xml_file.ajout_modules()
-        xml_file.ajout_notes()
-        xml_file.Creer_xml_GINF2()
-        xml_file.Creer_xml_Emploi()
-
-        print(ValidateXML.validate_with_dtd(xml_path_GINF2, dtd_path_GINF2))
-        print(ValidateXML.validate_with_xsd(xml_path_GINF2, xsd_path_GINF2))
-    except Exception as e:
-        print(f"Il y a une erreur: {e}")
+    # Call the function with the specified Excel and XML file paths
+    Creer_xml_GINF2(etudiant_xl_path, modules_xl_path, xml_path_GINF2)
+    Creer_xml_Emploi(emploi_xl_path, modules_xl_path, xml_path_Emploi)
